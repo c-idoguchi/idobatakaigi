96bde46f3554ddf40977c598a6e3ed4deb26ede4
c-idoguchi <h.idoguchi@psy-cholo.com>
2022-02-24 11:38:19 +0900

    makestyles,usestyles

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, {useState} from 'react';
 import Signin from './Signin';
 import Main from './Main'
+import config from '../config.json'
+
+
+console.log(config)
 
 export default () => {
   const [name, setName] = useState('');
   console.log({name});
 
-  if (name === '') {
+  if (config.signInEnabled && name === '') {
     return <Signin setName={setName} />;
   } else {
     return <Main name={name}/>
