8a63218112d06bd309f96704e80a48ca62a1c8c9
c-idoguchi <h.idoguchi@psy-cholo.com>
2022-02-25 10:45:24 +0900

    prop渡し、アバター画像

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,16 +1,28 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core'
+import { Avatar, Grid, makeStyles } from '@material-ui/core'
+import gravatarPath from '../gravatar'
 
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
+    margin: '26px'
   }
 })
 
-const MessageInputField = () => {
+const MessageInputField = ({name}) => {
   const classes = useStyles();
+  const avatarPath =gravatarPath(name)
+
   return (
-    <div className={classes.root}>MessageInputField</div>
+    <div className={classes.root}>
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar src={avatarPath}/>
+        </Grid>
+        <Grid xs={10}>入力</Grid>
+        <Grid xs={1}>ボタン</Grid>
+      </Grid>
+    </div>
   )
 }
 
