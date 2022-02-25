b718aae8f7904bcc859c690e5db692b8510855aa
c-idoguchi <h.idoguchi@psy-cholo.com>
2022-02-25 13:28:43 +0900

    gridに対するitemの設定

--- src/firebase.js
+++ src/firebase.js
@@ -14,10 +14,10 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref('messages')
+const messagesRef = database.ref('messages')
 
 export const pushMessage = ({name, text}) => {
-  messageRef.push({name, text})
+  messagesRef.push({name, text})
 };
 
 
