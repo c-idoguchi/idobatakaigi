2479032537d0a6497b2d707fd6c15efc45747d5c
c-idoguchi <h.idoguchi@psy-cholo.com>
2022-02-25 13:43:57 +0900

    .env.localを使用し、firebaseno

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,25 @@
 import firebase from 'firebase'
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID,
+} = process.env
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC7mV11eO14qrS-0ApRa0eOImACh6AtMUk",
-  authDomain: "idobatakaigi-with-ham-fo-5aee8.firebaseapp.com",
-  databaseURL: "https://idobatakaigi-with-ham-fo-5aee8-default-rtdb.firebaseio.com",
-  projectId: "idobatakaigi-with-ham-fo-5aee8",
-  storageBucket: "idobatakaigi-with-ham-fo-5aee8.appspot.com",
-  messagingSenderId: "11099453109",
-  appId: "1:11099453109:web:8aac98a5a3613b3b8130fc",
-  measurementId: "G-BEXT2EFEXV"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 
